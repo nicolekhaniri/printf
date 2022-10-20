7a149684fb22219f624011712544df320c7081cb
nicolekhaniri <nicolekhaniri@gmail.com>
2022-10-20 04:25:09 -0700

    commit2

--- printf.c
+++ printf.c
@@ -1,17 +1,23 @@
 #include <unistd.h>
+#include <stdio.h>
 
 int _printf(char *c);
 
 int _printf(char *c) {
 	int i;
+	char n = '\n';
+	int position = 0;
 
 	for (i = 0; c[i] != '\0'; i++) {
 		write(1, &c[i], 1);
+		position = position + 1;
 	}
-	return (0);
+	write(1, &n, 1);
+	return (position);
 }
 
-int main (void) {
-	_printf("Apple");
-	_printf("\n");
+int main (void) 
+{
+	_printf("Hello");
+	return (0);
 }
