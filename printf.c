2c7880bb29ad4dd73b2e0988ba1ef2d7a9bc18f4
nicolekhaniri <nicolekhaniri@gmail.com>
2022-10-20 07:38:21 -0700

    commit3

--- printf.c
+++ printf.c
@@ -1,29 +1,39 @@
 #include <unistd.h>
 #include <stdarg.h>
+/**
+ * _printf - Write a function that produces output according to a format
+ * @format: pointer to a string
+ * Return: number of printed characters
+ *
+ */
 
 int _printf(char *format, ...);
 
-int _printf(char *format, ...) {
+int _printf(char *format, ...)
+{
 	int i;
 	char n = '\n';
 	char space = ' ';
 	int position = 0;
 	va_list args;
+
 	va_start(args, format);
 
-	for (i = 0; format[i] != '\0'; i++) 
+	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] == ' ')
 		{
 			write(1, &space, 1);
 			position = position + 1;
 		}
-		else if(format[i] == '%')
+		else if (format[i] == '%')
 		{
 			int j = i + 1;
-			if(format[j] == 'c')
+
+			if (format[j] == 'c')
 			{
 				int x = va_arg(args, int);
+
 				write(1, &x, 1);
 				i += 1;
 				position += 1;
@@ -32,14 +42,14 @@ int _printf(char *format, ...) {
 		else
 		{
 			write(1, &format[i], 1);
-			position += 1;	
+			position += 1;
 		}
 	}
 	write(1, &n, 1);
 	return (position);
 }
 
-int main (void) 
+int main(void)
 {
 	_printf("Hello %c %c %c", 'y', 'o', 'u');
 	return (0);
